b36fcca9d67dc7bdcb2dc4e00f668d13c6339d72
Tomás Mársico <tmarsico@lenovo.com>
2023-12-02 16:47:06 -0300

    [challenge02] small fix

--- challenge02.ManejoDeArchivos/index.js
+++ challenge02.ManejoDeArchivos/index.js
@@ -1,18 +1,18 @@
-import { promises as fs } from 'fs';
-import { randomUUID } from 'crypto';
+import { promises as fs } from 'fs'
+import { randomUUID } from 'crypto'
 
 class ProductManager {
   constructor(path) {
-    this.path = path;
+    this.path = path
   }
 
   async getProducts() {
     try {
-      const content = JSON.parse(await fs.readFile(`./${this.path}`, 'utf-8'));
+      const content = JSON.parse(await fs.readFile(`./${this.path}`, 'utf-8'))
       return content
     } catch (error) {
-      console.log(error);
-      throw new Error(`Error reading products: ${error.message}`);
+      console.log(error)
+      throw new Error(`Error reading products: ${error.message}`)
     }
   }
 
@@ -25,13 +25,13 @@ class ProductManager {
       throw new Error('Invalid product description')
     }
     if (!price || typeof price !== 'number' || price <= 0) {
-      throw new Error('Invalid product price');
+      throw new Error('Invalid product price')
     }
     if (!stock || typeof stock !== 'number' || stock < 0) {
-      throw new Error('Invalid product stock');
+      throw new Error('Invalid product stock')
     }
     if (!thumbnail || !Array.isArray(thumbnail) || thumbnail.length === 0) {
-      throw new Error('Invalid product thumbnail');
+      throw new Error('Invalid product thumbnail')
     }
   }
 
@@ -39,71 +39,71 @@ class ProductManager {
     try {
       this.validateProductData(productData)
       const products = await this.getProducts()
-      const id = products.length + 1;
-      const code = randomUUID();
-      const newProduct = { id, code, ...productData };
-      products.push(newProduct);
-      await fs.writeFile(`./${this.path}`, JSON.stringify(products, null, 2));
-      console.log('New product added:', newProduct);
-      return products;
+      const id = products.length + 1
+      const code = randomUUID()
+      const newProduct = { id, code, ...productData }
+      products.push(newProduct)
+      await fs.writeFile(`./${this.path}`, JSON.stringify(products, null, "\t"))
+      console.log('New product added:', newProduct)
+      return products
     } catch (error) {
-      throw new Error(`Error adding product: ${error.message}`);
+      throw new Error(`Error adding product: ${error.message}`)
     }
   }
 
   async getProductById(productId) {
     try {
       const products = await this.getProducts()
-      const foundProduct = products.find((p) => p.id === productId);
-      return foundProduct ? [foundProduct] : console.log('Product not found.');
+      const foundProduct = products.find((p) => p.id === productId)
+      return foundProduct ? [foundProduct] : console.log('Product not found.')
     } catch (error) {
-      throw new Error(`Error getting product by ID: ${error.message}`);
+      throw new Error(`Error getting product by ID: ${error.message}`)
     }
   }
 
   async updateProductById(productId, productData) {
     try {
       const products = await this.getProducts()
-      const productIndex = products.findIndex((p) => p.id === productId);
+      const productIndex = products.findIndex((p) => p.id === productId)
       if (productIndex !== -1) {
         products[productIndex] = {
           ...products[productIndex],
           ...productData,
-        };
-        await fs.writeFile(`./${this.path}`, JSON.stringify(products, null, 2), 'utf-8');
-        console.log('Product updated successfully.');
+        }
+        await fs.writeFile(`./${this.path}`, JSON.stringify(products, null, "\t"), 'utf-8')
+        console.log('Product updated successfully.')
         return products
       } else {
-        console.log('Product not found.');
+        console.log('Product not found.')
         return null
       }
     } catch (error) {
-      throw new Error(`Error updating product by ID: ${error.message}`);
+      throw new Error(`Error updating product by ID: ${error.message}`)
     }
   }
 
   async deleteProductById(productId) {
     try {
       const products = await this.getProducts()
-      const productIndex = products.findIndex((p) => p.id === productId);
+      const productIndex = products.findIndex((p) => p.id === productId)
       if (productIndex === -1) {
-        throw new Error(`Product with ID ${productId} not found`);
+        throw new Error(`Product with ID ${productId} not found`)
       }
-      const productsFilt = products.filter((p) => p.id !== productId);
-      await fs.writeFile(`./${this.path}`, JSON.stringify(productsFilt, null, 2));
-      console.log(`Product with Id: ${productId} is deleted`);
+      const productsFilt = products.filter((p) => p.id !== productId)
+      await fs.writeFile(`./${this.path}`, JSON.stringify(productsFilt, null, "\t"))
+      console.log(`Product with Id: ${productId} is deleted`)
       return productsFilt
     } catch (error) {
-      throw new Error(`Error deleting product by ID: ${error.message}`);
+      throw new Error(`Error deleting product by ID: ${error.message}`)
     }
   }
 }
 
-const productManager = new ProductManager('products.json');
+const productManager = new ProductManager('products.json')
 
 
-// console.log(await productManager.getProducts());
-// console.log(await productManager.getProductById(11));
-// console.log(await productManager.deleteProductById(2));
-// productManager.addProduct({ title: "Peugeot", description: "Peugeot", price: 7000, stock: 10, thumbnail: ["https://yt3.ggpht.com/ytc/AMLnZu9VHYpPZl_WboTCenxYZtchOdCvzgy53zvLsOGYig=s88-c-k-c0x00ffffff-no-rj"] });
-// console.log(await productManager.updateProductById(3, { title: "Peugeot", description: "Peugeot", price: 7000, stock: 10, thumbnail: ["https://yt3.ggpht.com/ytc/AMLnZu9VHYpPZl_WboTCenxYZtchOdCvzgy53zvLsOGYig=s88-c-k-c0x00ffffff-no-rj"] }));
+// console.log(await productManager.getProducts())
+// console.log(await productManager.getProductById(11))
+// console.log(await productManager.deleteProductById(2))
+// productManager.addProduct({ title: "Peugeot", description: "Peugeot", price: 7000, stock: 10, thumbnail: ["https://yt3.ggpht.com/ytc/AMLnZu9VHYpPZl_WboTCenxYZtchOdCvzgy53zvLsOGYig=s88-c-k-c0x00ffffff-no-rj"] })
+// console.log(await productManager.updateProductById(3, { title: "Peugeot", description: "Peugeot", price: 7000, stock: 10, thumbnail: ["https://yt3.ggpht.com/ytc/AMLnZu9VHYpPZl_WboTCenxYZtchOdCvzgy53zvLsOGYig=s88-c-k-c0x00ffffff-no-rj"] }))
